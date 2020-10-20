26061cd09b078f3de2a872cf47342dcb2ac67bb3
Eric Young <eyoung88@frontier.com>
2020-10-20 13:04:21 -0600

    start of the row function

--- nimscript.js
+++ nimscript.js
@@ -1,1 +1,64 @@
-console.log("Working")
+
+class Match {
+    constructor(imageURI, isPickedUp) {
+        this.imageURI = imageURI;
+        this.isPickedUp = isPickedUp;
+    }
+}
+
+class MatchRow {
+    constructor(matches, matchesInRow, isEmpty) {
+        this.matches = matches;
+        this.matchesInRow = matchesInRow;
+        this.isEmpty = isEmpty;
+    }
+
+    pickUpMatches = (numberOfMatches) => {
+
+    }
+}
+
+let isPlaying = false;
+let matchRows = [];
+let selectedRow = undefined;
+let isBotTurn = false;
+
+class Game {
+    constructor(isPlaying, matchRows, selectedRow, isBotTurn) {
+        this.isPlaying = isPlaying;
+        this.matchRows = matchRows;
+        this.selectedRow = selectedRow;
+        this.isBotTurn = isBotTurn;
+
+        matchRows.push(new MatchRow([new Match("", false)], 1, false));
+        matchRows.push(new MatchRow([new Match("", false), new Match("", false), new Match("", false)], 3, false));
+        matchRows.push(new MatchRow([new Match("", false), new Match("", false), new Match("", false), new Match("", false), new Match("", false)], 5, false));
+        matchRows.push(new MatchRow([new Match("", false), new Match("", false), new Match("", false), new Match("", false), new Match("", false), new Match("", false), new Match("", false)], 7, false));
+    }
+
+    resetGame() {
+        matchRows.push(new MatchRow([new Match("", false)], 1, false));
+        matchRows.push(new MatchRow([new Match("", false), new Match("", false), new Match("", false)], 3, false));
+        matchRows.push(new MatchRow([new Match("", false), new Match("", false), new Match("", false), new Match("", false), new Match("", false)], 5, false));
+        matchRows.push(new MatchRow([new Match("", false), new Match("", false), new Match("", false), new Match("", false), new Match("", false), new Match("", false), new Match("", false)], 7, false));
+    }
+
+    isGameOver() {
+        return true;
+    }
+
+    doBotTurn() {
+
+    }
+
+    changeTurn() {
+
+    }
+}
+
+class Bot {
+    doTurn() {
+
+    }
+}
+
